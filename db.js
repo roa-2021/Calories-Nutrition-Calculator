cbf75fcef4072ccdddd0d210713763fcb0650b09
Gao Kang <gaokang@Gaos-MBP.lan>
2021-08-20 00:39:15 +1200

    little bit dbconn

--- db.js
+++ db.js
@@ -2,3 +2,15 @@
 const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
+
+const getUsers =(db = connection) =>{
+  return db('users').select()
+}
+
+const getUnits = (db = connection) =>{
+  return db('unit').select()
+}
+
+const getIngredientsNutritions = () =>{
+  return db('')
+}
