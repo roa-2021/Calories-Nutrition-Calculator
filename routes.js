59dfe6dec354215af9c8b9f4d52c7093f3a62890
Gao Kang <gaokang@Gaos-MBP.lan>
2021-08-19 20:29:25 +1200

    real blank repo

--- routes.js
+++ routes.js
@@ -3,4 +3,11 @@ const express = require('express')
 const db = require('./db')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  res.send("the first page")
+    .catch(err => {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
 module.exports = router;
