9d57c7055949a41aad665bf0df844f9fc367a9da
Casey <casey.dbnz@gmail.com>
2021-08-20 10:05:38 +1200

    added static css

--- routes.js
+++ routes.js
@@ -3,9 +3,10 @@ const db = require('./db')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('the first page').catch((err) => {
-    res.status(500).send('DATABASE ERROR: ' + err.message)
-  })
+  res.render('home')
+  // .catch((err) => {
+  //   res.status(500).send('DATABASE ERROR: ' + err.message)
+  // })
 })
 
 
