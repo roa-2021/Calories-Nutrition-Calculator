7007a5c4a6b85e5c414d998d56fd795970264a20
Gao Kang <gaokang@Gaos-MBP.lan>
2021-08-20 01:25:58 +1200

    add.js update

--- db.js
+++ db.js
@@ -3,19 +3,32 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
+//getting users
 const getUsers =(db = connection) =>{
   return db('users').select()
 }
 
+//getting units for dropping list 
 const getUnits = (db = connection) =>{
   return db('unit').select()
 }
 
-const getIngredientsNutritions = () =>{
+//getting mealTypes for dropping list
+const getMealType = (db = connection) => {
+  return db('meal').select()
+}
 
+//getting the nutritions of every ingridients
+const getIngredientsNutritions = (ingreID,db = connection) =>{
+  return db('ingredients')
+  .join('nutrition','nutrition.ingredients_id','nutrition.id')
+  .where('ingredients.id',ingreID)
+  .select()
 }
 
 module.exports = {
   getUsers:getUsers,
   getUnits:getUnits,
+  getIngredientsNutritions:getIngredientsNutritions,
+  getMealType:getMealType,
 }
