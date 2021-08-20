2e9c1f27fced3c31c68b4003ae1f4cb2d7a93325
Maximilon Baddeley <maximilon@Maximilons-iMac.local>
2021-08-20 14:11:17 +1200

    add outputMarkup

--- db.js
+++ db.js
@@ -1,15 +1,14 @@
-
 const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
 //getting users
-const getUsers =(db = connection) =>{
+const getUsers = (db = connection) => {
   return db('users').select()
 }
 
-//getting units for dropping list 
-const getUnits = (db = connection) =>{
+//getting units for dropping list
+const getUnits = (db = connection) => {
   return db('unit').select()
 }
 
@@ -18,26 +17,25 @@ const getMealType = (db = connection) => {
   return db('meal').select()
 }
 
-const getIngredientID = (IngreName,db = connection) =>{
+const getIngredientID = (IngreName, db = connection) => {
   return db('ingredients')
-  .where('ingredients.ingredients_name',IngreName)
-  .select('id')
+    .where('ingredients.ingredients_name', IngreName)
+    .select('id')
 }
 
-
 //getting the nutritions of every ingridients
-const getIngredientsNutritions = (ingreID,db = connection) =>{
+const getIngredientsNutritions = (ingreID, db = connection) => {
   console.log(ingreID)
   return db('nutrition')
-  // .join('nutrition','nutrition.ingredients_id','nutrition.id')
-  .where('ingredients_id',ingreID[0].id)
-  .select()
+    .join('ingredients', 'ingredients.id', 'nutrition.ingredients_id')
+    .where('ingredients_id', ingreID[0].id)
+    .select()
 }
 
 module.exports = {
-  getUsers:getUsers,
-  getUnits:getUnits,
+  getUsers: getUsers,
+  getUnits: getUnits,
   getIngredientID: getIngredientID,
-  getIngredientsNutritions:getIngredientsNutritions,
-  getMealType:getMealType,
+  getIngredientsNutritions: getIngredientsNutritions,
+  getMealType: getMealType,
 }
